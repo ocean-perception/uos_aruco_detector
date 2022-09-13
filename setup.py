ea11579ef0954a741d49f02069965ed520abed5a
miquelmassot <miquel.massot@gmail.com>
2022-09-13 16:43:38 +0100

    Fix typo

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ def run_setup():
         long_description = ""
     setup(
         name="uos_aruco_detector",
-        version="1.0.11",
+        version="1.0.12",
         install_requires=[
             "PyYAML",
             "pandas",
