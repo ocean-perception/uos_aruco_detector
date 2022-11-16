e3cc655f0b32a6d662759ca66b8ec54c38c7a902
Miquel Massot <miquel.massot@gmail.com>
2022-11-16 12:15:58 +0000

    Setup correct IP address

--- src/uos_aruco_detector/udp_broadcast_server.py
+++ src/uos_aruco_detector/udp_broadcast_server.py
@@ -2,6 +2,12 @@ import json
 import socket
 
 
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+
 class UDPBroadcastServer:
     def __init__(self, ip, port):
         # -- Enable port reusage
@@ -9,10 +15,22 @@ class UDPBroadcastServer:
             socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP
         )
         # -- Enable broadcasting mode
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        # -- Enable broadcasting mode
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.socket.settimeout(None)
         self.ip = ip
         self.port = port
+
+        current_ip = get_ip_address()
+        if self.ip != current_ip:
+            print(
+                "WARNING: The IP address of the broadcast server is different from the current IP address."
+            )
+            print(" - Current IP address: ", current_ip)
+            print(" - Broadcast server IP address: ", self.ip)
+            print("   Setting your IP address as broadcast server IP address.")
+            self.ip = current_ip
 
     def broadcast(self, message):
         # -- Broadcast the dictionary as a bytes-like object (string-like info) and empties
