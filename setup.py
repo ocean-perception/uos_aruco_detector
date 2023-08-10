f0a1c1cf686d55aea1f2e02161df4f3ee710f3c2
Miquel Massot <miquel.massot@gmail.con>
2023-08-10 16:34:59 +0100

    Bump opencv version

--- setup.py
+++ setup.py
@@ -85,8 +85,7 @@ def run_setup():
             "numpy==1.23.4",
             "matplotlib==3.5.1",
             "scipy==1.8.0",
-            "opencv-python==4.5.3.56",
-            "opencv-contrib-python>=4.5.3.56",
+            "opencv-contrib-python>=4.5.5.62",
         ],
         cmdclass={
             "develop": PostDevelopCommand,
