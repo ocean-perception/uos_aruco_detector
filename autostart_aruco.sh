14e59baa1361d85c4b2667c9157b9e7031998e15
Blair-insitu <B.Thornton@soton.ac.uk>
2025-02-28 10:54:47 +0000

    Add files via upload

---
+++ autostart_aruco.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wget -q  --spider http://www.google.com
+
+if [ $? -eq 0]; then
+	echo "Online"
+	pip install -U git+https://github.com/ocean-perception/uos_aruco_detector.git
+else
+	echo "Offline"
+fi
+sleep 60
+uos_aruco_detector
